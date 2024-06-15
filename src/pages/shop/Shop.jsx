7f277f11b62dced5fa953a6f12b1887313b5b1f9
rnrabi <rnrabi913@gmail.com>
2024-06-15 21:00:18 +0600

    pagenation is ok

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,4 +1,3 @@
-// import useAllMedicine from "../../hooks/useAllMedicine";
 import { FaEye } from "react-icons/fa";
 import useAxiosPublic from "../../hooks/useAxiosPublic";
 import { useEffect, useState } from "react";
@@ -23,10 +22,8 @@ const Shop = () => {
     const pages = [...Array(pageNumber).keys()].map(element => element + 1)
     console.log(pages)
 
-    // const [allMedicine, refetch] = useAllMedicine()
-    // console.log(allMedicine)
     const { data: allMedicine = [] } = useQuery({
-        queryKey: ['allMedicine', currentPage],
+        queryKey: ['allMedicine', currentPage, itemPerPage],
         queryFn: async () => {
             const res = await axiosPublic.get(`/allMedicine?page=${currentPage}&size=${itemPerPage}`)
             return res.data;
@@ -41,11 +38,6 @@ const Shop = () => {
         }
         getCountData()
     }, [])
-
-
-
-
-
 
 
 
@@ -107,10 +99,8 @@ const Shop = () => {
 
     // pagenation function 
     const handlePage = (value) => {
-        console.log(value)
+        // console.log(value)
         setCurrentPage(value)
-
-
     }
 
 
@@ -185,19 +175,23 @@ const Shop = () => {
 
             {/* pagenation */}
             <div className="flex justify-center space-x-1 dark:text-gray-800 my-12">
-                <button title="previous" type="button" className="inline-flex items-center justify-center w-8 h-8 py-0 border rounded-md shadow-md dark:bg-gray-50 dark:border-gray-100">
+                <button onClick={() => handlePage(currentPage - 1)}
+                    disabled={currentPage === 1}
+                    title="previous" type="button" className="inline-flex items-center justify-center w-8 h-8 py-0 border rounded-md shadow-md dark:bg-gray-50 dark:border-gray-100">
                     <svg viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round" className="w-4">
                         <polyline points="15 18 9 12 15 6"></polyline>
                     </svg>
                 </button>
 
                 {
-                    pages?.map(page => <button key={page} onClick={() => handlePage(page)} title="Page 1" className="inline-flex items-center justify-center w-8 h-8 text-sm font-semibold border rounded shadow-md dark:bg-gray-50 dark:text-violet-600 dark:border-violet-600">{page}</button>)
+                    pages?.map(page => <button key={page} onClick={() => handlePage(page)} title="Page 1" className={` ${currentPage === page ? 'bg-blue-500' : ''} inline-flex items-center justify-center w-8 h-8 text-sm font-semibold border rounded shadow-md dark:bg-gray-50 dark:text-violet-600 dark:border-violet-600`}>{page}</button>)
                 }
 
 
 
-                <button title="next" type="button" className="inline-flex items-center justify-center w-8 h-8 py-0 border rounded-md shadow-md dark:bg-gray-50 dark:border-gray-100">
+                <button onClick={() => handlePage(currentPage + 1)}
+                    disabled={currentPage === pageNumber}
+                    title="next" type="button" className="inline-flex items-center justify-center w-8 h-8 py-0 border rounded-md shadow-md dark:bg-gray-50 dark:border-gray-100">
                     <svg viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round" className="w-4">
                         <polyline points="9 18 15 12 9 6"></polyline>
                     </svg>
