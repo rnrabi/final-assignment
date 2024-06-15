0529b2f1ac6c7d7a8a3c32d6a6c5b41e9536a826
rnrabi <rnrabi913@gmail.com>
2024-06-16 00:53:01 +0600

    domain url chenge by server url

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 // import { useNavigate } from "react-router-dom";
 
 const axiosSecure = axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL: 'https://final-assignment-server-rho.vercel.app'
 })
 
 const useAxiosSecure = () => {
